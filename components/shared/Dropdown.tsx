8b94a8c64e0f917010e3e9372bbf49c9d8c81a80
Daniel <daniel_ye.nz@hotmail.com>
2023-12-29 22:07:03 -0800

    add dropdown menu

---
+++ components/shared/Dropdown.tsx
@@ -0,0 +1,41 @@
+"use client"
+
+import React, { useState } from "react";
+import {
+  Select,
+  SelectContent,
+  SelectItem,
+  SelectTrigger,
+  SelectValue,
+} from "@/components/ui/select";
+import { ICategory } from "@/lib/database/models/category.model";
+
+type DropdownProps = {
+  value?: string;
+  onChangeHandler?: () => void;
+};
+
+const Dropdown = ({ value, onChangeHandler }: DropdownProps) => {
+  const [categories, setCategories] = useState<ICategory[]>([
+    { _id: '1', name: 'Category 1'},
+    { _id: '2', name: 'Category 2'},
+    { _id: '3', name: 'Category 3'},
+  ])
+
+  return (
+    <Select onValueChange={onChangeHandler} defaultValue={value}>
+      <SelectTrigger className="select-field">
+        <SelectValue placeholder="Category" />
+      </SelectTrigger>
+      <SelectContent>
+        {categories.length > 0 && categories.map((category) => (
+          <SelectItem key={category._id} value={category._id} className="select-item p-regular-14">
+            {category.name}
+          </SelectItem>
+        ))}
+      </SelectContent>
+    </Select>
+  );
+};
+
+export default Dropdown;
